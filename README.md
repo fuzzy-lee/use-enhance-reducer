2b69dda5561f01f62508ffc01b3520eefd9dc0ba
li shiwen <lishiwen100@163.com>
2020-04-29 14:12:34 +0800

    add babel-loader

--- README.md
+++ README.md
@@ -2,7 +2,27 @@
 增强react useReducer支持dispatch一个函数
 
 
+### usage
+`npm install --save @lswlishiwen/use-enhance-reducer`
+
+与react的useReducer使用方法一致(将来会支持惰性初始化)，额外增加支持dispatch一个函数
+```javascript
+import {useEnhanceReducer} from "@lswlishiwen/use-enhance-reducer";
+
+//此处省略reducer、initState
+let [state, dispatch] = useEnhanceReducer(reducer, initState);
+
+let funAction = (dispatch, state) => {
+    setTimeout(() => {
+        dispatch({type: "ADD_TYPE"});
+    }, 1000)
+};
+
+dispatch(funAction)
+
+```
+
 
 ### TODO 
-1. babel编译
-2. 配置清理dist目录
+1. 支持惰性初始化（模拟重载实现）
+
--- script/webpack.config.js
+++ script/webpack.config.js
@@ -1,8 +1,26 @@
-var path = require('path');
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = function () {
     return {
         entry: "./src/index.js",
+        plugins: [
+            new CleanWebpackPlugin(),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        }
+                    }
+                }
+            ]
+        },
         output: {
             path: path.resolve(__dirname, '../dist'),
             filename: "useEnhanceReducer.js",
